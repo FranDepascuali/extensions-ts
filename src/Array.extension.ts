4f26f0d053a6356516abbb77f412ce024c5f1ece
Francisco Depascuali <depa.developer@gmail.com>
2024-05-01 22:56:29 +0200

    fix: incorrect code

--- src/Array.extension.ts
+++ src/Array.extension.ts
@@ -49,18 +49,3 @@ export namespace Arrays {
     });
   }
 }
-  export function withoutDuplicates<T, K>(
-    array: T[],
-    keyFn: (item: T) => K = (item: T) => item as unknown as K,
-  ): T[] {
-    const seen = new Set();
-    return array.filter((item) => {
-      const value = keyFn(item);
-      if (seen.has(value)) {
-        return false;
-      }
-      seen.add(value);
-      return true;
-    });
-  }
-}
